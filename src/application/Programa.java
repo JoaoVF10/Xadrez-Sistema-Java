fe3a6e16391ef499dc4416e547f3028b971a4285
João Vítor Ferreira <jv96516609@gmail.com>
2025-05-29 14:54:53 -0300

    Adição da classe posição

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Posicao pos=new Posicao(3,5);
+		
+		System.out.println(pos);
 
 	}
 
